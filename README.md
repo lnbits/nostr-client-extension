a24fe840dbc0e5a0e5df56ac9b06cb7673ef5b1d
callebtc <93376500+callebtc@users.noreply.github.com>
2023-02-10 14:30:04 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-We have made extensions actually installable, which means each extension gets its own repo, either in the lnbits org, or somewhere else, whichever is preferred. The process does remove the commit history, so its better if the extension author copies into the repo.
+# Nostr
+
+Opens a Nostr daemon
---
+++ migrations.py
@@ -0,0 +1,13 @@
+async def m001_initial(db):
+    """
+    Initial nostradmin table.
+    """
+    await db.execute(
+        f"""
+        CREATE TABLE nostradmin.relays (
+            id TEXT NOT NULL PRIMARY KEY,
+            url TEXT NOT NULL,
+            active BOOLEAN DEFAULT true
+        );
+    """
+    )
